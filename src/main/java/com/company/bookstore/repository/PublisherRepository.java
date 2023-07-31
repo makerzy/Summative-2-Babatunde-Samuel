c7d0987ba673ebf9bf378f6b79bd7dc232abbb09
makerzy <botassammzy@gmail.com>
2023-07-31 16:03:45 -0700

    added pkg and files

---
+++ src/main/java/com/company/bookstore/repository/PublisherRepository.java
@@ -0,0 +1,6 @@
+package com.company.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Integer, PublisherRepository> {
+}
