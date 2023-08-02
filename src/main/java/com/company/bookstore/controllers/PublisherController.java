790787577f83402192df00a176583ba66da8e6ef
kcast03 <karlacastello48@gmail.com>
2023-08-01 19:21:01 -0500

    Created application.properties Added publisher controller test

--- src/main/java/com/company/bookstore/controllers/PublisherController.java
+++ src/main/java/com/company/bookstore/controllers/PublisherController.java
@@ -1,4 +1,48 @@
 package com.company.bookstore.controllers;
 
+import com.company.bookstore.models.Publisher;
+import com.company.bookstore.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class PublisherController {
+
+    @Autowired
+    PublisherRepository repo;
+
+    //create publisher - POST
+    @PostMapping("/publishers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Publisher addPublisher(@RequestBody Publisher publisher){return repo.save(publisher);}
+
+
+    //read by id - GET by id
+    @GetMapping("/publishers/{id}")
+    public Publisher getPublisherById(@PathVariable Integer id){
+        Optional<Publisher> returnVal = repo.findById(id);
+        if(returnVal.isPresent()){
+            return returnVal.get();
+        }else{
+            return null;
+        }
+    }
+
+    //read all - GET all
+    @GetMapping("/publishers")
+    public List<Publisher> getPublishers(){return repo.findAll();}
+
+    //update - PUT
+    @PutMapping("/publishers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePublisher(@RequestBody Publisher publisher){repo.save(publisher);}
+
+    //delete
+    @DeleteMapping("/publishers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePublisher(@PathVariable Integer id){repo.deleteById(id);}
 }
--- src/main/java/com/company/bookstore/models/Publisher.java
+++ src/main/java/com/company/bookstore/models/Publisher.java
@@ -1,4 +1,122 @@
 package com.company.bookstore.models;
 
-public class Publisher {
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "publisher")
+public class Publisher implements Serializable {
+    @Id
+    @Column(name = "publisher_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "publisherId")
+    private Set<Book> books = new HashSet<>();
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String phone;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(getId(), publisher.getId()) && Objects.equals(getName(), publisher.getName()) && Objects.equals(getStreet(), publisher.getStreet()) && Objects.equals(getCity(), publisher.getCity()) && Objects.equals(getState(), publisher.getState()) && Objects.equals(getPostalCode(), publisher.getPostalCode()) && Objects.equals(getPhone(), publisher.getPhone()) && Objects.equals(getEmail(), publisher.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getStreet(), getCity(), getState(), getPostalCode(), getPhone(), getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
